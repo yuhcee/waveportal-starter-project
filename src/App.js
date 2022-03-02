163ff5beb3e30a1fa4d3f978119ca2006d5a07a6
yuhcee <egbouchenna001@gmail.com>
2022-03-02 17:08:31 +0100

    add checkIfWalletIsConnected

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
 export default function App() {
+    /*
+     * Just a state variable we use to store our user's public wallet.
+     */
+    const [currentAccount, setCurrentAccount] = useState('');
 
-  const wave = () => {
-    
-  }
-  
-  return (
-    <div className="mainContainer">
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = typeof window !== undefined && window;
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
+            if (!ethereum) {
+                console.log('Make sure you have metamask Installed!');
+                return;
+            } else {
+                console.log('We have the ethereum object', ethereum);
+            }
+
+            /*
+             * Check if we're authorized to access the user's wallet
+             */
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log('Found an authorized account:', account);
+                setCurrentAccount(account);
+            } else {
+                console.log('No authorized account found');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    return (
+        <div className="mainContainer">
+            <div className="dataContainer">
+                <div className="header">
+                    <span role="img" aria-label="wave">
+                        👋{' '}
+                    </span>
+                    Hey there!
+                </div>
+                <div className="bio">I am Yuhcee and I am learning about blockchain and cryptocurrencies. That's pretty cool right? Connect your Ethereum(Metamask) wallet and wave at me!</div>
+                <button className="waveButton" onClick={''}>
+                    Wave at Me
+                </button>
+            </div>
         </div>
-
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
